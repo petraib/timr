d9b2b1a4c91d001d3e49a6fed98e24c02f70cc69
Raghu Betina <raghu@firstdraft.com>
2017-08-22 00:30:50 +0000

    Whitelist columns in Source for ActiveAdmin

--- app/admin/source.rb
+++ app/admin/source.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Source do
 
+ permit_params :author_id, :name, :url, :indicator_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
