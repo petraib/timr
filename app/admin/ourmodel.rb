66146968e087cb2fb10c8d977a35cb9a40b546e3
Raghu Betina <raghu@firstdraft.com>
2017-08-22 00:30:58 +0000

    Whitelist columns in Ourmodel for ActiveAdmin

--- app/admin/ourmodel.rb
+++ app/admin/ourmodel.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Ourmodel do
 
+ permit_params :model_weight, :ourmodel_name
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
