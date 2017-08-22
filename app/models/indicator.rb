aa894be1b5ac8e1b3d06c1f8179f74ee0006d2d5
Raghu Betina <raghu@firstdraft.com>
2017-08-22 00:30:14 +0000

    Direct association between indicators and variables

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -1,6 +1,9 @@
 class Indicator < ApplicationRecord
   # Direct associations
 
+  has_many   :variables,
+             :dependent => :destroy
+
   has_many   :sources,
              :dependent => :destroy
 
