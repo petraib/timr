68018fc1e27700470b9333b32b26e97f219b057a
Raghu Betina <raghu@firstdraft.com>
2017-08-22 00:30:54 +0000

    Whitelist columns in Variable for ActiveAdmin

--- app/admin/variable.rb
+++ app/admin/variable.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Variable do
 
+ permit_params :indicator_id, :model_id, :weight, :ourmodel_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
